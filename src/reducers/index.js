6d1ff6b1edfac0274bdb15c1ba08deab29d41418
danko-m <dankomilutinovic@gmail.com>
2017-03-08 20:40:06 +0100

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+const initState = {
+  result: 0,
+  increment: 1
+}
+
+export default (state = initState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state, 
+        result: state.result + state.increment
+      }
+    case 'DECREMENT':
+      return {
+        ...state,
+         result: state.result - state.increment
+      }
+    case 'RESET_COUNTER':
+      return {
+        ...state,
+         result: 0
+      }
+    case 'CHANGE_INCREMENT':
+      return {
+        ...state,
+         increment: action.increment
+      }
+    case 'RESET_INCREMENT':
+      return {
+        ...state,
+         increment: 1
+      }
+    default:
+      return state
+  }
+}
